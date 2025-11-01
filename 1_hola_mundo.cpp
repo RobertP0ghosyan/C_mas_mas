fdb5c9eaa9f906ba34560b90dd4717fe0aa30396
RobertP0ghosyan <robert292000@gmail.com>
2025-11-01 17:16:18 +0100

    initial commit

---
+++ 1_hola_mundo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hola, Mundo!\n";
+    cout << "Este es un programa simple en C++.";
+
+    int x=10,y=20;
+    cout<<"la suma es "<<x+y<<endl;
+
+    float b=10.5;
+    cout <<"El valor flaot de b es: "<< b <<endl;
+
+    string name="Juan";
+    cout<<"Hola, "<<name<<"!"<<endl;
+
+    bool isActive= true;
+    cout << "Alumno activo: " << (isActive ? "Si" : "No") << endl;
+    
+    return 0;
+
+}  
