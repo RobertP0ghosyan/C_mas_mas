51719767370c21b08fb59e52af6316c52e6d2722
RobertP0ghosyan <robert292000@gmail.com>
2025-11-03 16:25:21 +0100

    el uso de IF

---
+++ 8_IF.cpp
@@ -0,0 +1,29 @@
+//como funciona el if
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+    int i=10;
+
+    if (i!=0){
+
+        cout<< "el valor de i: " <<i<<endl;
+        
+
+
+    }else if (i==1){
+
+
+
+        cout<< "el valor de i es 1"<<endl;
+    }else{
+
+
+
+        cout<< "el valor de i es 0"<<endl;
+    }
+    
+    return 0;
+}
